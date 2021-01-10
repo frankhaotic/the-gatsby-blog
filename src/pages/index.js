645ca93500653f1be45ecd7c89b5713e4069c563
Frankie Flood <fsfloody@gmail.com>
2021-01-10 12:27:04 +0000

    more changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>The Great Gatsby Bootcamp!</div>
+const IndexPage = () => {
+  return(
+    <div>
+      <h1>Hello,</h1>
+      <h2>I am a second header</h2>
+    </div>
+  )
 }
+
+export default IndexPage
