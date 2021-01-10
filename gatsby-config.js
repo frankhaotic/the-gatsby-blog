fc4ba4dc6be1df044a6e4fe504e88d278edf9689
Frankie Flood <fsfloody@gmail.com>
2021-01-10 16:56:59 +0000

    more changes god bless

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Frankie Flood'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ]
--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,41 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+
+// import './header.module.scss' 
+import headerStyles from './header.module.scss'
 
 const Header = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
     return (
-        <header>
-            <h1>Frankie Site</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to='/'>
+                   {data.site.siteMetadata.title}
+                </Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link to='/'>Index</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>Index</Link>
                     </li>
                     <li>
-                        <Link to='/blog'>Blog </Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>Blog </Link>
                     </li>
                     <li>
-                        <Link to='/contact'> Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'> Contact</Link>
                     </li>
                     <li>
-                        <Link to='/about'>About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>About</Link>
                     </li>
                 </ul>
             </nav>
