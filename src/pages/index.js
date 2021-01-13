f01da37f5f3fa09a89f1f7c0ae41906afb5f9c2b
Frankie Flood <fsfloody@gmail.com>
2021-01-13 17:21:21 +0000

    clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
       <Head title='Home' />
       <h1>Hello,</h1>
       <h2>I am a second header</h2>
-      <p>Need a developer? <Link to='/contact'>Contact me.</Link></p>
     </Layout>
   )
 }
